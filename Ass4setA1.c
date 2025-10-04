840dfe33c34b178117d237973f5898b7a05b2bf7
BCS20230 <sivi10272005@gmail.com>
2025-10-04 19:16:21 +0530

    Add files via upload

---
+++ Ass4setA1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+int i,j,n,a[50],frame[10],no,k,avail,count=0;
+printf("\n Enter The Number Of Pages:\n");
+
+scanf("%d",&n);
+printf("\nEnter the page number (reference string):\n");
+for(i=1;i<=n;i++)
+scanf("%d",&a[i]);
+printf("\n Enter The number of frames:");
+scanf("%d",&no);
+for(i=0;i<no;i++)
+ 
+ frame[i]=-1;
+ j=0;
+ printf("\tRefstring\tpage frames\n");
+ for(i=1;i<=n;i++)
+ {
+ printf("%d\t\t",a[i]);
+ avail=0;
+ for(k=0;k<no;k++)
+ if(frame[k]==a[i])
+ avail=1;
+ if(avail==0)
+ {
+ frame[j]=a[i];
+ j=(j+1)%no;
+ count++;
+ for(k=0;k<no;k++)
+ printf("%d\t",frame[k]);
+ }
+ printf("\n");
+ }
+ printf("Page Fault is %d",count);
+ return 0;
+ }
